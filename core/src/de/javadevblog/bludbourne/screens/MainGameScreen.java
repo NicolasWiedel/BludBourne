6184e4ca88996e1c82c90f7a7fd51cb7fd7da9f6
Nicolas Wiedel <nwiedel@alte-hof-apotheke.com>
2021-02-26 08:47:08 +0100

    2.3.1. MainGameScreen beginning

---
+++ core/src/de/javadevblog/bludbourne/screens/MainGameScreen.java
@@ -0,0 +1,11 @@
+package de.javadevblog.bludbourne.screens;
+
+import com.badlogic.gdx.Screen;
+
+public class MainGameScreen implements Screen{
+	public static final String TAG = MainGameScreen.class.getSimpleName();
+	
+	public static class VIEWPORT {
+		
+	}
+}
