9cdbfa6c8933c647fd35284284e3e2253f541d16
NicolasWiedel <nicolaswiedel@googlemail.com>
2021-03-11 22:32:39 +0100

    3.3.5.Final

--- desktop/src/de/javadevblog/bludbourne/desktop/DesktopLauncher.java
+++ desktop/src/de/javadevblog/bludbourne/desktop/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		
+		// config for BludBourne
 		config.title = "BludBourne";
 		config.useGL30 = false;
 		config.width = 800;
